dbd7c2088069d0cf3e8e8e4f4a199e825eab61b6
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-07-07 00:34:38 +0530

    Details of Profile.js are hardcoded for now

--- client/src/MyComponents/profile.js
+++ client/src/MyComponents/profile.js
@@ -2,12 +2,6 @@ import React from "react";
 import "./profile.css";
 import virat_kohli from "../myImages/virat kohli.jpg";
 const Profile = () => {
-  // var authData = localStorage.getItem("authUser");
-  // var authUser = JSON.parse(authData);
-  // const authUserName = authUser.displayName;
-  // const authUserEmail = authUser.email;
-  // const userID = authUserName.split(" ")[0];
-
   return (
     <>
       <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
@@ -16,17 +10,17 @@ const Profile = () => {
             <button class="btn btn-secondary">
               <img src={virat_kohli} height="120" width="120" alt="userImage" />
             </button>
-            {/* <span class="name mt-3">{authUserName}</span> */}
-            {/* <span class="emailId">{authUserEmail}</span> */}
+            <span class="name mt-3">Virat Kohli</span>
+            <span class="emailId">kohlivirat@gmail.com</span>
             <div class="d-flex flex-row justify-content-center align-items-center gap-2">
-              {/* <span class="user">@{userID}</span>{" "} */}
+              <span class="user">@Virat129</span>{" "}
               <span>
                 <i class="fa fa-copy"></i>
               </span>
             </div>
             <div class="d-flex flex-row justify-content-center align-items-center mt-3">
               <span class="number">
-                1069 <span class="follow">Followers</span>
+                240 Million <span class="follow">Followers</span>
               </span>
             </div>
             <div class="text mt-3">
@@ -34,20 +28,6 @@ const Profile = () => {
                 Crickter.
                 <br />
                 Best player of the year.
-              </span>
-            </div>
-            <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
-              <span>
-                <i class="fa fa-twitter"></i>
-              </span>
-              <span>
-                <i class="fa fa-facebook-f"></i>
-              </span>
-              <span>
-                <i class="fa fa-instagram"></i>
-              </span>
-              <span>
-                <i class="fa fa-linkedin"></i>
               </span>
             </div>
             <div class=" px-2 rounded mt-4 date ">
