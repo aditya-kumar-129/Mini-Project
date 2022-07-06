5f5f670812b89d6d85b1b70dd2636df07c2d5edd
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-07-06 20:23:19 +0530

    chat.js

--- client/src/MyComponents/chat.js
+++ client/src/MyComponents/chat.js
@@ -9,14 +9,13 @@ class KommunicateChat extends Component{
     componentDidMount(){
         (function(d, m){
             var kommunicateSettings = 
-                {"appId":"3627177d7a759e8968c983631270b79bb","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                {"appId":"3e28c50b27db8c6baccce72a158621a0c","popupWidget":true,"automaticChatOpenOnNavigation":true};
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
             window.kommunicate = m; m._globals = kommunicateSettings;
         })(document, window.kommunicate || {});
     }
-    
     render(){
         return(
             
