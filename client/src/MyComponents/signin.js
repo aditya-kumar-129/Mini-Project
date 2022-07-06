964cf05b88ce56b08018329ee82695de606ee166
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-07-06 13:37:44 +0530

    Logo added

--- client/src/MyComponents/signin.js
+++ client/src/MyComponents/signin.js
@@ -1,92 +1,102 @@
-import React , {useState} from 'react';
-import {useHistory} from 'react-router-dom'
-import img from '../myImages/sportify.jpeg';
-
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import img from "../myImages/sportify.jpg";
 
 export default function Signin() {
- 
-    const history = useHistory()
-    const [password,setPasword] = useState("")
-    const [email,setEmail] = useState("")
+  const history = useHistory();
+  const [password, setPasword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const PostData = ()=>{
-        var data = JSON.stringify({
-            password,
-            email});
-            console.log(data)
-        fetch("/signin",{
-            method:"post",
-            headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-               },
-        
-            body:data
-        })
-        .then(data=>{
-           if(data.error){
-               window.alert(data.error)
-           }
-           else{
-               history.push('/home')
-           }
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
+  const PostData = () => {
+    var data = JSON.stringify({
+      password,
+      email,
+    });
+    console.log(data);
+    fetch("/signin", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
 
-    return (
-        <div className="container ms-auto me-auto mt-5 mb-5">
-            <section className="mb-4">
-                <div className="row">
+      body: data,
+    })
+      .then((data) => {
+        if (data.error) {
+          window.alert(data.error);
+        } else {
+          history.push("/home");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-                    <div className="col-lg-9 mb5 mt-5">
-                        <img src={img} className="img-responsive mt-5" alt="..." />
-                    </div>
+  return (
+    <div className="container ms-auto me-auto mt-5 mb-5">
+      <section className="mb-4">
+        <div className="row">
+          <div className="col-lg-9 mb5 mt-5">
+            <img src={img} className="img-responsive mt-5" alt="..." />
+          </div>
 
-                    <div className="col-lg-3 mb-5">
-                        <h1 className="h1-responsive fs-1 fw-bold text-center">Sign In</h1>
-                        <form  action=''>
-                            <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="text" id="email" name="email" className="form-control" placeholder='Email'
-                                            autoComplete='off'
-                                        value={email}
-                                        onChange={(e)=>setEmail(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="password" id="password" name="password" className="form-control" placeholder='Password'
-                                            autoComplete='off'
-                                            value={password}
-                                            onChange={(e) => setPasword(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-
-                        <div className="d-grid gap-2 mb-5">
-                            <button className="btn btn-primary" type="submit"
-                            onClick={() => PostData()}
-                            >Submit</button>
-                        </div>
-                        <div className="status"></div>
-                        <span>Don't have an account yet    </span>
-                        <a className="btn btn-dark" href="signup">  Sign Up</a>
-                  
-                    </div>
-
-
+          <div className="col-lg-3 mb-5">
+            <h1 className="h1-responsive fs-1 fw-bold text-center">Sign In</h1>
+            <form action="">
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="text"
+                      id="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Email"
+                      autoComplete="off"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
                 </div>
+              </div>
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      autoComplete="off"
+                      value={password}
+                      onChange={(e) => setPasword(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
 
-            </section>
+            <div className="d-grid gap-2 mb-5">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                onClick={() => PostData()}
+              >
+                Submit
+              </button>
+            </div>
+            <div className="status"></div>
+            <span>Don't have an account yet </span>
+            <a className="btn btn-dark" href="signup">
+              {" "}
+              Sign Up
+            </a>
+          </div>
         </div>
-    
-       );
-}; 
+      </section>
+    </div>
+  );
+}
--- client/src/MyComponents/signup.js
+++ client/src/MyComponents/signup.js
@@ -1,112 +1,125 @@
-import React, { useState } from 'react';
-import {useHistory} from 'react-router-dom'
-import img from '../myImages/sportify.jpeg';
-
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import img from "../myImages/sportify.jpg";
 
 export default function Signup() {
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [password, setPasword] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
 
-    const history = useHistory()
-    const [name,setName] = useState("")
-    const [password,setPasword] = useState("")
-    const [email,setEmail] = useState("")
-    const [username,setUsername] = useState("")
+  const PostData = () => {
+    var data = JSON.stringify({
+      name,
+      username,
+      password,
+      email,
+    });
+    console.log(data);
+    fetch("/signup", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
 
-    const PostData = ()=>{
-        var data = JSON.stringify({
-            name,
-            username,
-            password,
-            email});
-            console.log(data)
-        fetch("/signup",{
-            method:"post",
-            headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-               },
-        
-            body:data
-        })
-        .then(data=>{
-           if(data.error){
-               window.alert(data.error)
-           }
-           else{
-               history.push('/signin')
-           }
-        }).catch(err=>{
-            console.log(err)
-        })
-    }
+      body: data,
+    })
+      .then((data) => {
+        if (data.error) {
+          window.alert(data.error);
+        } else {
+          history.push("/signin");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div className="container ms-auto me-auto mt-5 mb-5">
-            <section className="mb-4">
-                <div className="row">
+  return (
+    <div className="container ms-auto me-auto mt-5 mb-5">
+      <section className="mb-4">
+        <div className="row">
+          <div className="col-lg-9 mb5 mt-5">
+            <img src={img} className="img-responsive mt-5" alt="..." />
+          </div>
 
-                    <div className="col-lg-9 mb5 mt-5">
-                        <img src={img} className="img-responsive mt-5" alt="..." />
-                    </div>
-
-                    <div className="col-lg-3 mb-5">
-                        <h1 className="h1-responsive fs-1 fw-bold text-center">Sign Up</h1>
-                        <form action=''>
-                        <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="text" name="name" placeholder='Name' autoComplete="off"
-                                        value={name}
-                                        onChange={(e)=>setName(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="text" name="email"  placeholder='Email' autoComplete="off"
-                                        value={email}
-                                        onChange={(e)=>setEmail(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="text" name="username"  placeholder='username' autoComplete="off"
-                                        value={username}
-                                        onChange={(e)=>setUsername(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row mt-5 mb-5">
-                                <div className="col-md-12">
-                                    <div className="md-form mb-0">
-                                        <input type="password"  name="password"  placeholder='Password' autoComplete="off"
-                                        value={password}
-                                        onChange={(e)=>setPasword(e.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-    
-                        </form>
-
-                        <div className="d-grid gap-2 mb-5">
-                            <button className="btn btn-primary" type="submit"
-                            onClick={() => PostData()}
-                            >Submit</button>
-                        </div>
-                        <div className="status"></div>
-                    </div>
-
-
+          <div className="col-lg-3 mb-5">
+            <h1 className="h1-responsive fs-1 fw-bold text-center">Sign Up</h1>
+            <form action="">
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      autoComplete="off"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
                 </div>
+              </div>
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder="Email"
+                      autoComplete="off"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="username"
+                      autoComplete="off"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-5 mb-5">
+                <div className="col-md-12">
+                  <div className="md-form mb-0">
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      autoComplete="off"
+                      value={password}
+                      onChange={(e) => setPasword(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
 
-            </section>
+            <div className="d-grid gap-2 mb-5">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                onClick={() => PostData()}
+              >
+                Submit
+              </button>
+            </div>
+            <div className="status"></div>
+          </div>
         </div>
-    
-       );
-  }; 
+      </section>
+    </div>
+  );
+}
