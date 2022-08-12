219cb18dae4f247c50a540dd5bfbf64e2c193aba
aditya-kumar-129 <adityakumar93340@gmail.com>
2022-08-12 13:25:18 +0530

    fixed the header issue

--- client/src/MyComponents/header.js
+++ client/src/MyComponents/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../myImages/sportify.jpg";
 import "./Styles.css";
+import styles from "./header.module.css";
 
 export default function Header() {
   const signOutHandler = () => {
@@ -8,66 +9,77 @@ export default function Header() {
   }
   
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-light">
-      <div className="container-fluid">
-        <a
-          className="nav-link active navbar-brand"
-          aria-current="page"
-          href="home"
-        >
-          <img src={logo} alt="..." width="250px" height="80px" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item ms-2 me-2">
-              <a className="nav-link active" aria-current="page" href="home">
-                <span className="nva">Home</span>
-              </a>
-            </li>
+    <div className={styles.header}>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+        <div className="container-fluid">
+          <a
+            className="nav-link active navbar-brand"
+            aria-current="page"
+            href="home"
+          >
+            <img src={logo} alt="..." width="250px" height="80px" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item ms-2 me-2">
+                <a className="nav-link active" aria-current="page" href="home">
+                  <span className="nva">Home</span>
+                </a>
+              </li>
 
-            <li className="nav-item ms-2 me-2">
-              <a className="nav-link active" aria-current="page" href="profile">
-                <span className="nva">Profile</span>
-              </a>
-            </li>
-            <li className="nav-item ms-2 me-2">
-              <a className="nav-link active" aria-current="page" href="ocr">
-                <span className="nva">Search Substances</span>
-              </a>
-            </li>
+              <li className="nav-item ms-2 me-2">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="profile"
+                >
+                  <span className="nva">Profile</span>
+                </a>
+              </li>
+              <li className="nav-item ms-2 me-2">
+                <a className="nav-link active" aria-current="page" href="ocr">
+                  <span className="nva">Search Substances</span>
+                </a>
+              </li>
 
-            <li className="nav-item ms-2 me-2">
-              <a
-                className="nav-link active"
-                href="http://localhost:7000/discussion"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="nva">Discussions</span>
-              </a>
-            </li>
+              <li className="nav-item ms-2 me-2">
+                <a
+                  className="nav-link active"
+                  href="http://localhost:7000/discussion"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="nva">Discussions</span>
+                </a>
+              </li>
 
-            <li className="nav-item ms-2 me-2">
-              <a className="nav-link active" aria-current="page" href="about">
-                <span className="nva">About US</span>
-              </a>
-            </li>
-            <li className="nav-item ms-2 me-2">
-              <a className="nav-link active" aria-current="page" href="signin" onClick={signOutHandler}>
-                <span className="nva">Sign Out</span>
-              </a>
-            </li>
-          </ul>
+              <li className="nav-item ms-2 me-2">
+                <a className="nav-link active" aria-current="page" href="about">
+                  <span className="nva">About US</span>
+                </a>
+              </li>
+              <li className="nav-item ms-2 me-2">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="signin"
+                  onClick={signOutHandler}
+                >
+                  <span className="nva">Sign Out</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
